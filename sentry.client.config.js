21dd2a37b7fe2a84d8faffc7d906eb84e0d469ec
Wei Su <suv@cyberconnect.me>
2022-09-23 02:22:51 +0800

    feat: release

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -12,7 +12,7 @@ Sentry.init({
     "https://a11cb72da661421f97abd56d6047125f@o1421660.ingest.sentry.io/6767643",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
-  release: "1.8.0",
+  release: process.env.SENTRY_RELEASE,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
