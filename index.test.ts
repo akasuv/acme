d54ed7c3dfb968b072426ab6ae3937892f9b5344
Wei Su <suv@cyberconnect.me>
2022-09-22 15:17:22 +0800

    feat: test Sentry

--- index.test.ts
+++
@@ -1,6 +0,0 @@
-const { add } = require("./add");
-
-test("add two numbers", () => {
-  const res = add(1, 2);
-  expect(res).toBe(3);
-});
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import "../styles/globals.css";
-import { init, getUserId } from "@amplitude/analytics-browser";
 import React from "react";
-
-init("05a03498729f408269c4eff6e4c92db3");
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
