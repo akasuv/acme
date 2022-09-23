edd60868de848f923a0c4fdda701d7145d906c34
Wei Su <suv@cyberconnect.me>
2022-09-23 15:09:17 +0800

    feat: alpha test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   const handleClick = () => {
-    throw new Error("Test Sentry");
+    throw new Error("Test Sentry alpha");
   };
   return <div onClick={handleClick}>div</div>;
 }
