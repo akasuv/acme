5a670c0702ed093a8abaf2871a8e8b27cc7c1d81
Wei Su <suv@cyberconnect.me>
2022-09-23 14:26:38 +0800

    feat: test6

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const moduleExports = {
     disableServerWebpackPlugin: true,
     disableClientWebpackPlugin: true,
   },
+  productionBrowserSourceMaps: true,
 };
 
 const sentryWebpackPluginOptions = {
