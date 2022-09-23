8f081da77ed5bc02ca4c60dc85ffdfbaae3939df
Wei Su <suv@cyberconnect.me>
2022-09-23 13:41:56 +0800

    feat: test 3

--- next.config.js
+++ next.config.js
@@ -9,7 +9,10 @@ const moduleExports = {
   // Your existing module.exports
   // /** @type {import('next').NextConfig} */
   reactStrictMode: true,
-  sentry: {},
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+  },
 };
 
 const sentryWebpackPluginOptions = {
@@ -26,4 +29,4 @@ const sentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = withSentryConfig(moduleExports);
