50e864c1265d9d885ff67b397301f54a66626f3a
Wei Su <suv@cyberconnect.me>
2022-09-25 23:46:00 +0800

    feat: update release

--- next.config.js
+++ next.config.js
@@ -19,6 +19,7 @@ const sentryWebpackPluginOptions = {
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
+  release: process.env.NEXT_PUBLIC_SENTRY_RELEASE,
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
