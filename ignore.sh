2a81531430f77860edf0ae7be18bc4b6a6ff8d24
Wei Su <suv@cyberconnect.me>
2022-09-26 00:56:31 +0800

    feat: tes

--- ignore.sh
+++ ignore.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo $VERCEL_ENV
-echo $NEXT_PUBLIC_SENTRY_RELEASE
-if [["$abc" == ''"]]; then
-	exit 1;
+echo "VERCEL_ENV: $VERCEL_ENV"
+
+if [[ "$VERCEL_ENVx" == "" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed $VEVERCEL_ENVx"
+  exit 1;
 
 else
-	exit 0;
-
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
 fi
