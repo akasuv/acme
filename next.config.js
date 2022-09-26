2d01acc2ead49c3fb5390aae9224dc2483312a6a
Wei Su <suv@cyberconnect.me>
2022-09-26 16:59:41 +0800

    feat: f

--- next.config.js
+++ next.config.js
@@ -23,6 +23,10 @@ const sentryWebpackPluginOptions = {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.package
   release: process.env.NEXT_PUBLIC_SENTRY_RELEASE,
+  setCommits: {
+    auto: true,
+    ignoreEmpty: true,
+  },
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
